db798c4859f5af1f641809893799e42432cce669
wilfred.lim <wilfred.lim@ixen.io>
2022-09-16 16:08:43 +0800

    hashmap and linkedlist

--- a1_The_LeetCode_Beginners_Guide.java
+++ a1_The_LeetCode_Beginners_Guide.java
@@ -156,4 +156,73 @@ public class a1_The_LeetCode_Beginners_Guide {
         // Return the result
         return steps;
     }
+
+    // 6 - 876. Middle of the Linked List
+    // Given the head of a singly linked list, return the middle node of the linked list.
+    // If there are two middle nodes, return the second middle node.
+    public ListNode middleNode(ListNode head) {
+
+        // Initialize the middle node, the node that keep moving until the endNodes stop it
+        ListNode middleNode = head;
+
+        // Initialize the end node, the end node is for telling the middle node that there're still space to continue moving
+        ListNode endNode = head;
+
+        // Loop until the endNode gets to the end
+        while(endNode != null && endNode.next != null){
+
+            // Move the middleNode up by one step
+            middleNode = middleNode.next;
+
+            // Move the endNode up by two steps
+            endNode = endNode.next.next;
+        }
+
+        // Return the result
+        return middleNode;
+    }
+
+    // 7 - 383. Ransom Note
+    // Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+    // Each letter in magazine can only be used once in ransomNote.
+    public boolean canConstruct(String ransomNote, String magazine) {
+
+        // Initialize the magazineMap for storing the characters and its count
+        HashMap<Character, Integer> magazineMap = new HashMap<>();
+
+        // Loop for each chars in magazine
+        for(int i = 0; i < magazine.length(); i++){
+
+            // Store the current character
+            Character a = magazine.charAt(i);
+
+            // Get the count of the character in the map, default to 0
+            int currentCount = magazineMap.getOrDefault(a, 0);
+
+            // Add the character in the map with the current count + 1
+            magazineMap.put(a, currentCount + 1);
+        }
+
+        // Loop for each chars in ransomNote
+        for(int j = 0; j < ransomNote.length(); j++){
+
+            // Check the current character
+            Character b = ransomNote.charAt(j);
+
+            // Get the count of the character in the map
+            int currentCt = magazineMap.getOrDefault(b, 0);
+
+            // If the count is 0, return false
+            if(currentCt == 0) {
+                return false;
+            }
+
+            // Else continue the operation by removing the char count from the map
+            magazineMap.put(b, currentCt - 1);
+        }
+
+        // Return true if manage to reach here
+        return true;
+    }
+}
 }
